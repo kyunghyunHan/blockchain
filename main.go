849b52c7adae03467e6e4ec3273247c7ec47915b
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-11-24 00:06:35 +0900

    http

--- main.go
+++ main.go
@@ -1,30 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	blockchain "github.com/kyunghyun/blockchain/block"
-)
-
-const port string = ":4000"
-
-type homeDate struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.gohtml"))
-	data := homeDate{"Home", blockchain.GetBlockchain().AllBlocks()}
-	tmpl.Execute(rw, data)
-}
+import "github.com/kyunghyun/blockchain/explorer"
 
 func main() {
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
 
+	explorer.Start()
 }
