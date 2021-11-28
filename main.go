66e14844a7356a58bed61e54b5564dbd9d7c067e
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-11-29 01:04:28 +0900

    db

--- main.go
+++ main.go
@@ -4,5 +4,7 @@ import blockchain "github.com/kyunghyun/blockchain/block"
 
 func main() {
 
-	blockchain.Blockchain()
+	blockchain.Blockchain().AddBlock("First")
+	blockchain.Blockchain().AddBlock("Second")
+	blockchain.Blockchain().AddBlock("Third")
 }
